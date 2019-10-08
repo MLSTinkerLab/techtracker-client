8f61de3d85adc9bb9a108e353accff57579a88a6
Lauren Auchter <lsauchter@gmail.com>
2019-10-08 18:19:04 -0400

    Updated landing page instruction

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -12,8 +12,16 @@ export default function LandingPage() {
             <Link to="/checkin" className="checkLink"><button className="buttonLink">Check In</button></Link>
         </div>
         <div className="info">
-            <p>Thanks for using TechTracker! Click the links above to enter your name and select the items you're checking in or out. Head to the admin dashboard to see how the app data is maintained.
-            </p>
+            <h3>How to</h3>
+            <ul className="bulletpoint">
+                <li>Click the link above</li>
+                <li>Select your name</li>
+                <li>Click item group(s)</li>
+                <li>Select the quantity</li>
+                <li>Hit Check In/Out!</li>
+            </ul>
+            <p>You can check in or out as many different items as you need</p>
+            
         </div>
         <div>
             {confirmation}
